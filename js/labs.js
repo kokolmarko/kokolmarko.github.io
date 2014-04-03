09acb0ebd6b86fcf044b4e8f32bdead5330e08fd
Semantika Build Server <info@semantika.si>
2014-04-03 12:27:48 +0200

    Autocommit

--- js/labs.js
+++ js/labs.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
 
 	semLabs.initMenu();
 	
+	try{
+		convertEmails();
+		}catch(e){
+			//alert(e.message);
+		}
+
+	
 	$("#homepage").parent().attr("id", "homepagebody");
 	
 	$("#hamMenu").click(function(){
@@ -19,10 +26,63 @@ $(document).ready(function(){
 	
 	$("#homepage ul.nav-footer").append("<br class='clear'/>");
 	
+	if(document.referrer.indexOf("/search") != -1 && document.URL.indexOf("/search")==-1){
+		//$("#search").append("<a style='display:block; font-size:0.8em; text-align:center;' href='"+document.referrer+"'>Back to search results</a>");
+		
+		$("div.tabs ul").prepend("<li>"+"<a href='"+document.referrer+"'>← Back to search results</a>"+"</li>");
+	}
+	
+	/*$("ul.tags li a").click(function(){
+		alert("OK");
+		$("ul.tags li a").removeClass("current-tag");
+		$(this).addClass("current-tag");
+		$("div.pagination-holder").jPages("destroy");
+		$("ul.post-list li").removeClass("filtered-by-tag").hide();
+		$("ul.post-list li."+$(this).attr("rel")).addClass("filtered-by-tag").show();
+	});*/
+	
+	$("a.datasetstag-at-item").click(function(){
+		$("ul.tags li a[rel='"+$(this).attr("rel")+"']").click();
+	});	
+	
+	$(".obfuscate").each(function(){
+		var currentMailto = $(this).attr("href");
+		$(this).attr("href", currentMailto.replace("[at]","@"));
+		
+		var currentText = $(this).text();
+		$(this).text(currentText.replace("[at]","@"));
+	});
+		
+	/*
+	$("div.startBtn a").click(function (event) {
+	    event.preventDefault();
+	    //calculate destination place
+	    var dest = 0;
+	    if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+	        dest = $(document).height() - $(window).height();
+	    } else {
+	        dest = $(this.hash).offset().top;
+	    }
+	    //go to destination
+	    $('html,body').animate({
+	        scrollTop: dest
+	    }, 2000, 'swing');
+	});
+	*/
+	if($("#homepage").length <= 0){
+		$("footer").attr("id", "nothomepagefooter");
+	}
+	
 	//select gallery tag if selected
+	var splitedGalleryUrl = window.location.pathname.split('/');
+	if( splitedGalleryUrl.length>2){
+		$("ul.tags li a[rel='"+splitedGalleryUrl[2]+"']").addClass("current-tag");
+	}
 	
 
-	initApiMenu();
+	sortTagsListOneLevel();
+	
+		initApiMenu();
 		 
 });
 
