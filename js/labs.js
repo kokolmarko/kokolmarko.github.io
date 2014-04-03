533af4ccaeae13e021b327c271250f4ed8cc19e0
Semantika Build Server <info@semantika.si>
2014-04-03 12:20:03 +0200

    Autocommit

--- js/labs.js
+++ js/labs.js
@@ -1,16 +1,88 @@
 $(document).ready(function(){
 
+	semLabs.initMenu();
 	
+	try{
+		convertEmails();
+		}catch(e){
+			//alert(e.message);
+		}
+
 	
+	$("#homepage").parent().attr("id", "homepagebody");
+	
+	$("#hamMenu").click(function(){
+		if($("#nav").hasClass("menu-opened")){
+			$("#nav").css("max-height","0");
+			$("#nav").removeClass("menu-opened");
+			$("#homepage #banner").show();
+		}else{
+			
+			$("#nav").css("max-height","999px");
+			$("#nav").addClass("menu-opened");
+			$("#homepage #banner").hide();
+		}
+	});
+	
+	$("#homepage ul.nav-footer").append("<br class='clear'/>");
+	
+	if(document.referrer.indexOf("/search") != -1 && document.URL.indexOf("/search")==-1){
+		//$("#search").append("<a style='display:block; font-size:0.8em; text-align:center;' href='"+document.referrer+"'>Back to search results</a>");
+		
+		$("div.tabs ul").prepend("<li>"+"<a href='"+document.referrer+"'>← Back to search results</a>"+"</li>");
+	}
+	
+	/*$("ul.tags li a").click(function(){
+		alert("OK");
+		$("ul.tags li a").removeClass("current-tag");
+		$(this).addClass("current-tag");
+		$("div.pagination-holder").jPages("destroy");
+		$("ul.post-list li").removeClass("filtered-by-tag").hide();
+		$("ul.post-list li."+$(this).attr("rel")).addClass("filtered-by-tag").show();
+	});*/
+	
+	$("a.datasetstag-at-item").click(function(){
+		$("ul.tags li a[rel='"+$(this).attr("rel")+"']").click();
+	});	
+	
+	$(".obfuscate").each(function(){
+		var currentMailto = $(this).attr("href");
+		$(this).attr("href", currentMailto.replace("[at]","@"));
+		
+		var currentText = $(this).text();
+		$(this).text(currentText.replace("[at]","@"));
+	});
+		
+	/*
+	$("div.startBtn a").click(function (event) {
+	    event.preventDefault();
+	    //calculate destination place
+	    var dest = 0;
+	    if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+	        dest = $(document).height() - $(window).height();
+	    } else {
+	        dest = $(this.hash).offset().top;
+	    }
+	    //go to destination
+	    $('html,body').animate({
+	        scrollTop: dest
+	    }, 2000, 'swing');
+	});
+	*/
+	if($("#homepage").length <= 0){
+		$("footer").attr("id", "nothomepagefooter");
+	}
 	
 	//select gallery tag if selected
 	var splitedGalleryUrl = window.location.pathname.split('/');
 	if( splitedGalleryUrl.length>2){
 		$("ul.tags li a[rel='"+splitedGalleryUrl[2]+"']").addClass("current-tag");
 	}
+	
+
 	sortTagsListOneLevel();
 	
-	initApiMenu();
+		initApiMenu();
 		 
 });
 
@@ -99,6 +171,25 @@ function sortTagsListOneLevel(){
 
 function initApiMenu()
 {
+			
+			$("ul a.menuTrigger").click(function(e){
+				//alert("OK");
+				$(this).parent().find("ul.notabs").slideToggle();
+				return e.preventDefault();
+			});
+			
+			
+			$("ul.notabs li a").removeClass("current");
+			var baseUrl = $("a.logo").attr("href").toString();
+			var currentUrl = window.location.pathname;
+			
+			$("ul.notabs li a").each(function(){
+				if(currentUrl.match("^"+$(this).attr("href")+"(/?)$")){
+					$(this).addClass("current");
+					$(this).parent().parent().parent().find("a:first").click();
+				}
+			});	
+
 
 }
 
